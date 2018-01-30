b120242f91984ec89ae32d15f54b92179e73c987
JP12 <j.d.peterik@gmail.com>
2018-01-29 19:30:50 -0600

    Add Custom Countdown
    Make more modular, and custom countdowns

--- code.js
+++ code.js
@@ -1,248 +1,295 @@
 /*jshint maxerr: 1000 */
 
+function startTimer() {
+  getEndDate();
+}
 
-function startTimer(delay, PLC) {
-    var hours;
+function getEndDate() {
+  var hours;
+  var now = new Date(Date.now() + delay);
 
-    if (!delay) delay = 0;
-    if (!PLC) PLC = false;
+  if (window.PLC) {
+    hours = [45, 33, 20, [7, 54], [23, 52], 21, 7, 53];
+  } else {
+    hours = [45, 40, 34, 28, [22, 51], [20, 49], 43, 37];
+  }
 
-    if (PLC) {
-        hours = [45, 33, 20, [7, 54],
-            [23, 52], 21, 7, 53
-        ];
-    } else {
-        hours = [45, 40, 34, 28, [22, 51],
-            [20, 49], 43, 37
-        ];
-    }
-    var now = new Date(Date.now() + delay * 1000);
+  var endHour = now.getHours();
 
-    var endHour = now.getHours();
+  var currentMinute = now.getMinutes();
 
-    var currentMinute = now.getMinutes();
+  var period = endHour - 7;
 
-    var period = endHour - 7;
+  if (period < 0) {
+    alert("It's before school!");
+    return;
+  }
 
-    var endMinuteTest = hours[period];
+  var endMinuteTest = hours[period];
 
-    if (period < 0) {
-      alert('It\'s before school!');
-      return;
-    }
+  if (!endMinuteTest) {
+    alert("It's after school!");
+    return;
+  }
 
-    if (!endMinuteTest) {
-      alert('It\'s after school!');
-      return;
-    }
+  var endMinuteTestArray;
 
-    var endMinuteTestArray;
+  var isArray = false;
 
-    var isArray = false;
+  var arrayNum;
 
-    var arrayNum;
+  if (Array.isArray(endMinuteTest)) {
+    endMinuteTestArray = endMinuteTest;
 
-    if (Array.isArray(endMinuteTest)) {
-        endMinuteTestArray = endMinuteTest;
+    endMinuteTest = endMinuteTestArray[0];
 
-        endMinuteTest = endMinuteTestArray[0];
+    isArray = true;
 
-        isArray = true;
-
-        arrayNum = 0;
-
-        if (currentMinute >= endMinuteTest) {
-            endMinuteTest = endMinuteTestArray[1];
-            arrayNum = 1;
-        }
-    }
+    arrayNum = 0;
 
     if (currentMinute >= endMinuteTest) {
-        endHour++;
+      endMinuteTest = endMinuteTestArray[1];
+      arrayNum = 1;
+    }
+  }
 
-        period = endHour - 7;
+  if (currentMinute >= endMinuteTest) {
+    endHour++;
 
-        endMinuteTest = hours[period];
+    period = endHour - 7;
 
-        isArray = false;
+    endMinuteTest = hours[period];
+
+    if (!endMinuteTest) {
+      alert("It's after school!");
+      return;
     }
 
-    if (Array.isArray(endMinuteTest)) {
-        endMinuteTestArray = endMinuteTest;
+    isArray = false;
+  }
 
-        endMinute = endMinuteTestArray[0];
+  if (Array.isArray(endMinuteTest)) {
+    endMinuteTestArray = endMinuteTest;
 
-        isArray = true;
-        arrayNum = 0;
-    } else {
-        endMinute = endMinuteTest;
-    }
+    endMinute = endMinuteTestArray[0];
 
-    countdown(endHour, endMinute, delay, period, isArray, arrayNum, PLC);
+    isArray = true;
+    arrayNum = 0;
+  } else {
+    endMinute = endMinuteTest;
+  }
+
+  var endDate = new Date(Date.now() + window.delay);
+  endDate.setHours(endHour);
+
+  endDate.setMinutes(endMinute);
+
+  endDate.setSeconds(0);
+  message = getMessage(period, isArray, arrayNum);
+
+  loop(endDate, message);
 }
 
-function countdown(endHour, endMinute, delay, period, isArray, arrayNum, PLC) {
-    var longMessages;
+function getMessage(period, isArray, arrayNum) {
+  var messages;
 
-    if (PLC) {
-        longMessages = [
-            'until start of school',
-            'left in period 1',
-            'left in period 2', ['left in period 3', 'left in period 4'],
-            ['left in lunch A', 'left in lunch B'],
-            'left in lunch C',
-            'left in period 6',
-            'left in period 7',
-        ];
-    } else {
-        longMessages = [
-            'until start of school',
-            'left in period 1',
-            'left in period 2',
-            'left in period 3', ['left in period 4', 'left in lunch A'],
-            ['left in lunch B', 'left in lunch C'],
-            'left in period 6',
-            'left in period 7',
-        ];
-    }
+  if (window.PLC) {
+    messages = [
+      'until start of school',
+      'left in period 1',
+      'left in period 2',
+      ['left in period 3', 'left in period 4'],
+      ['left in lunch A', 'left in lunch B'],
+      'left in lunch C',
+      'left in period 6',
+      'left in period 7',
+    ];
+  } else {
+    messages = [
+      'until start of school',
+      'left in period 1',
+      'left in period 2',
+      'left in period 3',
+      ['left in period 4', 'left in lunch A'],
+      ['left in lunch B', 'left in lunch C'],
+      'left in period 6',
+      'left in period 7',
+    ];
+  }
 
-    var longMessage = longMessages[period];
+  var message = messages[period];
 
-    if (isArray) {
-        longMessage = longMessage[arrayNum];
-    }
+  if (isArray) {
+    message = message[arrayNum];
+  }
 
-    display(endHour, endMinute, delay, longMessage, PLC);
+  return message;
 }
 
-function display(endHour, endMinute, delay, longMessage, PLC) {
-    var now = new Date(Date.now() + delay * 1000);
+function loop(endDate, message) {
+  now = new Date(Date.now() + window.delay);
 
-    var endDate = new Date();
+  var diff = endDate - now;
 
-    endDate.setHours(endHour);
+  var hours = Math.floor(diff / 3.6e6);
 
-    endDate.setMinutes(endMinute);
+  var minutes = Math.floor(diff % 3.6e6 / 6e4);
 
-    endDate.setSeconds(0);
+  var seconds = Math.floor(diff % 6e4 / 1000);
 
-    var diff = endDate - now;
+  if (seconds < 0) {
+    run();
+    return;
+  }
 
-    var hours = Math.floor(diff / 3.6e6);
+  timeLeft =
+    hours + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
 
-    var minutes = Math.floor(diff % 3.6e6 / 6e4);
+  if (hours === 0) timeLeft = timeLeft.substring(2);
 
-    var seconds = Math.floor(diff % 6e4 / 1000);
+  setText(timeLeft, 'Clock');
 
-    if (seconds < 0) {
-        startTimer(delay, PLC);
-        return;
-    }
-
-    timeLeft = hours + ':' + ("0" + minutes).slice(-2) + ':' + ("0" + seconds).slice(-2);
-
-    if (hours === 0) timeLeft = timeLeft.substring(2)
-
-    setText(timeLeft, 'Clock');
-
-    document.title = (timeLeft + ' ' + longMessage);
-    window.stopID = setTimeout(display, 1000, endHour, endMinute, delay, longMessage, PLC);
+  document.title = (timeLeft + ' ' + message);
+  // console.log(timeLeft + ' ' + message);
+  window.stopID = setTimeout(loop, 1000, endDate, message, PLC);
 }
 
 function getWednesdays() {
-    'use strict';
-    var todaygetWed = new Date();
-    var month = todaygetWed.getMonth();
-    var wednesdays = [];
+  'use strict';
+  var todaygetWed = new Date(Date.now() + window.delay);
+  var month = todaygetWed.getMonth();
+  var wednesdays = [];
 
-    todaygetWed.setDate(1);
-    todaygetWed.setHours(0, 0, 0, 0);
+  todaygetWed.setDate(1);
+  todaygetWed.setHours(0, 0, 0, 0);
 
-    while (todaygetWed.getDay() !== 3) {
-        todaygetWed.setDate(todaygetWed.getDate() + 1);
-    }
+  while (todaygetWed.getDay() !== 3) {
+    todaygetWed.setDate(todaygetWed.getDate() + 1);
+  }
 
-    while (todaygetWed.getMonth() === month) {
-        wednesdays.push(new Date(todaygetWed.getTime()));
-        todaygetWed.setDate(todaygetWed.getDate() + 7);
-    }
+  while (todaygetWed.getMonth() === month) {
+    wednesdays.push(new Date(todaygetWed.getTime()));
+    todaygetWed.setDate(todaygetWed.getDate() + 7);
+  }
 
-    return wednesdays;
+  return wednesdays;
 }
 
-function isPLC() {
-    'use strict';
-    var todayPLC = new Date();
-    if (todayPLC.getDay != 3) return false;
+function isPLC(date) {
+  'use strict';
+  if (date.getDay != 3) return false;
 
-    var wednesdays = getWednesdays();
-    var wednesdayOne = wednesdays[0];
-    var wednesdayThree = wednesdays[2];
-    if (
-        todayPLC.getDate() == wednesdayOne.getDate() ||
-        todayPLC.getDate() == wednesdayThree.getDate()) {
-        return true;
-    }
-    return false;
+  var wednesdays = getWednesdays();
+  var wednesdayOne = wednesdays[0];
+  var wednesdayThree = wednesdays[2];
+  return (
+    date.getDate() == wednesdayOne.getDate() ||
+    date.getDate() == wednesdayThree.getDate()
+  );
 }
 
-function setText(text,ID) {
-    document.getElementById(ID).innerHTML = text;
+function setText(text, ID) {
+  document.getElementById(ID).innerHTML = text;
+  // console.log(text);
 }
 
 function numberFormat(numberStr) {
-
-    if (numberStr.substring(-2) === '11') {
-      return 'th';
-    } else if (numberStr.substring(-1) === '1') {
-        return 'st';
-    } else if (numberStr.substring(-1) === '2') {
-        return 'nd';
-    } else if (numberStr.substring(-1) === '3') {
-        return 'rd';
-    } else {
-        return 'th';
-    }
+  if (numberStr.substr(-2) === '11') {
+    return 'th';
+  } else if (numberStr.substr(-2) === '12') {
+    return 'th';
+  } else if (numberStr.substr(-2) === '13') {
+    return 'th';
+  } else if (numberStr.substr(-1) === '1') {
+    return 'st';
+  } else if (numberStr.substr(-1) === '2') {
+    return 'nd';
+  } else if (numberStr.substr(-1) === '3') {
+    return 'rd';
+  } else {
+    return 'th';
+  }
 }
 
-function isUnusual() {
-    today = new Date();
-    todayStr = today.getMonth() + '/' + today.getDate() + '/' + today.getFullYear();
-    unusualDays = ['1/26/2018'];
-    return unusualDays.indexOf(todayStr) != -1;
+function isUnusual(date) {
+  dateStr =
+    date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear();
+
+  unusualDays = ['1/26/2018'];
+  return unusualDays.indexOf(dateStr) != -1;
 }
 
-function run(delay) {
-
-    if (window.stopID) clearTimeout(window.stopID);
-
-    var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var today = new Date();
-    var dayMessage = 'Today is ' + weekdays[today.getDay()] + ', ' + months[today.getMonth()] + ' ' + today.getDate() + numberFormat('' + today.getDate()) + ', ';
-
-
-
-    if (today.getDay() === 6 || new Date().getDay() === 0) {
-        alert('It\'s the weekend. Why are you here?');
-        dayMessage = dayMessage + 'and it is the weekend.';
-        setText(dayMessage, 'Date');
-        return;
-    } else if (isUnusual()) {
-        alert('Today has a unique schedule. Countdown won\'t work today. Sorry.');
-        dayMessage = dayMessage + 'and it is an unusual day.';
-        setText(dayMessage, 'Date');
-        return;
-    } else if (isPLC()) {
-        alert('Today is a PLC day. Hurray!');
-        dayMessage = dayMessage + 'and it is a PLC day.';
-        setText(dayMessage, 'Date');
-        startTimer(delay, true);
-    } else {
-        dayMessage = dayMessage + 'and it is a regular weekday.';
-        setText(dayMessage, 'Date');
-        startTimer(delay);
-    }
+function formatDate(date) {
+  var weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return (
+    weekdays[date.getDay()] +
+    ', ' +
+    months[date.getMonth()] +
+    ' ' +
+    date.getDate() +
+    numberFormat('' + date.getDate())
+  );
 }
 
-run(parseInt(localStorage.getItem('delay')));
+function run(endDate) {
+  if (window.stopID) clearTimeout(window.stopID);
+
+  delay = parseInt(localStorage.getItem('delay'));
+  // window.delay = 0;
+  window.delay = window.delay * 1000;
+
+  today = new Date(Date.now() + delay);
+  dayMessage = 'Today is ' + formatDate(today) + ', ';
+
+  window.PLC = isPLC(today);
+
+  if (endDate) {
+    alert('Custom countdown set!');
+    dayMessage = dayMessage + 'and you are running a custom countdown.';
+    setText(dayMessage, 'Date');
+    loop(endDate, 'left');
+  } else if (today.getDay() === 6 || today.getDay() === 0) {
+    alert("It's the weekend. Why are you here?");
+    dayMessage = dayMessage + 'and it is the weekend.';
+    setText(dayMessage, 'Date');
+    return;
+  } else if (isUnusual(today)) {
+    alert("Today has a unique schedule. Countdown won't work today. Sorry.");
+    dayMessage = dayMessage + 'and it is an unusual day.';
+    setText(dayMessage, 'Date');
+    return;
+  } else if (window.PLC) {
+    alert('Today is a PLC day. Hurray!');
+    dayMessage = dayMessage + 'and it is a PLC day.';
+    setText(dayMessage, 'Date');
+    startTimer(delay);
+  } else {
+    dayMessage = dayMessage + 'and it is a regular weekday.';
+    setText(dayMessage, 'Date');
+    startTimer(delay);
+  }
+}
+
+run();
