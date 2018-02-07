973ac1a6b7d113d088c89f3c723518df66a6c821
jpeterik12 <31190190+jpeterik12@users.noreply.github.com>
2018-02-06 21:14:04 -0600

    Add dynamic corners

--- general/clock.js
+++ general/clock.js
@@ -1,20 +1,40 @@
+function clockMove() {
+  if (this.style.top == '0px') {
+    if (this.style.right == '0px') {
+      this.style.top = 'calc(100% - ' + this.offsetHeight + 'px)';
+      this.onmouseover = null;
+      setTimeout(function(diver){ diver.onmouseover = clockMove }, 1000, this);
+    } else {
+      this.style.right = '0px';
+      this.onmouseover = null;
+      setTimeout(function(diver){ diver.onmouseover = clockMove }, 1000, this);
+    }
+  } else {
+    if (this.style.right == '0px') {
+      this.style.right = 'calc(100% - ' + this.offsetWidth + 'px)';
+      this.onmouseover = null;
+      setTimeout(function(diver){ diver.onmouseover = clockMove }, 1000, this);
+    } else {
+      this.style.top = '0px';
+      this.onmouseover = null;
+      setTimeout(function(diver){ diver.onmouseover = clockMove }, 1000, this);
+    }
+  }
+}
+
+
+
 function createClock() {
   var deleter = document.getElementById("clock-div");
   if (deleter) deleter.parentNode.removeChild(deleter);
   
   clockdiv = document.createElement('div');
-  clockdiv.innerHTML = '<p style="position: fixed; top: 0; right: 0; font-family:monospace; padding: 0 0 0 0; font-size: 1.5em; z-index: 99999999999999999999999; border: 1px solid #000000; background-color: white" id=clock>00:00</p>';
-  clockdiv.style = 'position: fixed; top: 0; right: 0; padding: 0 0 0 0;';
+  clockdiv.innerHTML = '<p style="font-family:monospace; padding: 0 0 0 0; font-size: 20px; z-index: 99999999999999999999999; border: 1px solid #000000; background-color: white" id=clock>00:00</p>';
+  clockdiv.style = 'position: fixed; top: 0px; right: 0px; padding: 0 0 0 0; transition: all 1s ease-out;';
   clockdiv.id = 'clock-div';
   
   
-//   clockdiv.onmouseover = function(){
-//     document.getElementById('clock').style.display = 'none';
-//   };
-  
-//   clockdiv.onmouseout = function(){
-//     document.getElementById('clock').style.display = 'inline';
-//   };
+  clockdiv.onmouseover = clockMove;
   
   document.body.appendChild(clockdiv);
 }
