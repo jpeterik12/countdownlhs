79f47f9e764ef7a54e4b9ee62af2a4e81f35b831
JP12 <j.d.peterik@gmail.com>
2018-02-11 20:29:44 -0600

    Schedule maker

--- general/clock.js
+++ general/clock.js
@@ -3,5 +3,5 @@ if (window.location.hostname == 'www.countdownlhs.ga') {
   alert("Drag me to your bookmarks bar. If I'm already there, use me on another site!");
 } else {
   document.body.appendChild(document.createElement("script")).src="https://www.countdownlhs.ga/javascript/common.js";
-  document.body.appendChild(document.createElement("script")).src="https://www.countdownlhs.ga/javascript/bookmarklet.js";
+  document.body.appendChild(document.createElement("script")).src="https://www.countdownlhs.ga/javascript/mobilebookmarklet.js";
 }
