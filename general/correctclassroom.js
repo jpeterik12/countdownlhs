87df531f68858bd81117f5c3be23a3ca6e928ad4
JP12 <j.d.peterik@gmail.com>
2018-04-16 21:17:43 -0500

    Callie

--- general/correctclassroom.js
+++ general/correctclassroom.js
@@ -1,6 +1,12 @@
 if (window.location.hostname == 'www.countdownlhs.ga') {
   alert("Drag me to your bookmarks bar. If I'm already there, use me on another site!");
 } else {
+  alert(cuteMessage());
   document.body.appendChild(document.createElement("script")).src="https://www.countdownlhs.ga/javascript/common.js";
   document.body.appendChild(document.createElement("script")).src="https://www.countdownlhs.ga/javascript/callie.js";
+}
+
+
+function cuteMessage() {
+  return 'Hey, I\'ve got a question: Would you rather have a small amount of handwritten messages to show up whenever you use this, or a random one from a long list of generated ones?'
 }
