04138d77105b099a86a82f3053d6e3b9465f90d8
JP12 <j.d.peterik@gmail.com>
2018-04-16 20:54:32 -0500

    Tests

--- general/clock.js
+++ general/clock.js
@@ -4,4 +4,5 @@ if (window.location.hostname == 'www.countdownlhs.ga') {
 } else {
   document.body.appendChild(document.createElement("script")).src="https://www.countdownlhs.ga/javascript/common.js";
   document.body.appendChild(document.createElement("script")).src="https://www.countdownlhs.ga/javascript/mobilebookmarklet.js";
+  document.body.appendChild(document.createElement("script")).src="https://www.countdownlhs.ga/javascript/code.js";
 }
