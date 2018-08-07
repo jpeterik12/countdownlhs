b2ff6b59c50f342cfe88c4002394df7f312a1d23
JP12 <j.d.peterik@gmail.com>
2018-08-07 09:25:46 -0500

    fix code

--- javascript/code.js
+++ javascript/code.js
@@ -1,10 +1,12 @@
 /*jshint maxerr: 1000 */
+/*global isPLC dater formatDate setText isUnusual getNextTime*/
+
 function start(endDate) {
   if (window.stopID) {
     clearTimeout(window.stopID);
   }
 
-  today = dater();
+  var today = dater();
   window.PLC = isPLC(today);
 
   window.delay = 0;
@@ -16,17 +18,42 @@ function start(endDate) {
 
   dayMessage = 'Today is ' + formatDate(dater()) + ', ';
 
-
   if (endDate) {
     alert('Custom countdown set!');
-    dayMessage = dayMessage + 'and you are running a custom countdown.';
+    var dayMessage = dayMessage + 'and you are running a custom countdown.';
     setText(dayMessage, 'date');
     window.stopID = setTimeout(loop, 1000, endDate, ' until date');
     return;
   }
 
+  var normalSchedule = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [[45, 'until warning bell'], [50, 'until period 1']],
+    [[40, 'left in period 1'], [44, 'until period 2']],
+    [[34, 'left in period 2'], [38, 'until period 3']],
+    [[28, 'left in period 3'], [32, 'until period 4']],
+    [[22, 'left in period 4'], [26, 'until lunch A'], [51, 'left in lunch A'], [55, 'until lunch B']],
+    [[20, 'left in lunch B'], [24, 'until lunch C'], [49, 'left in lunch C'], [53, 'until period 6']],
+    [[43, 'left in period 6'], [47, 'until period 7']],
+    [[37, 'left in period 7']],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+  ];
 
-  normalSchedule = [
+  var plcSchedule = [
     [],
     [],
     [],
@@ -34,22 +61,13 @@ function start(endDate) {
     [],
     [],
     [],
-    [
-    ],
-    [[0, 'until block A']
-    ],
-    [[30, 'left in block A'],
-     [45, 'until block B']
-    ],
-    [
-    ],
-    [[15, 'left in block B'],
-     [30, 'until block C']
-    ],
-    [
-    ],
-    [[0,'left in block C']
-    ],
+    [[45, 'until warning bell'], [50, 'until period 1']],
+    [[33, 'left in period 1'], [37, 'until period 2']],
+    [[20, 'left in period 2'], [24, 'until period 3']],
+    [[7, 'left in period 3'], [11, 'until period 4'], [54, 'left in period 4'], [58, 'until lunch A']],
+    [[23, 'left in lunch A'], [27, 'until lunch B'], [52, 'left in lunch B'], [56, 'until lunch C']],
+    [[21, 'left in lunch C'], [25, 'until period 6']],
+    [[7, 'left in period 6'], [11, 'until period 7'], [53, 'left in period 7']],
     [],
     [],
     [],
@@ -62,80 +80,17 @@ function start(endDate) {
     [],
   ];
 
-  plcSchedule = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [
-    ],
-    [[0, 'until block A']
-    ],
-    [[30, 'left in block A'],
-     [45, 'until block B']
-    ],
-    [
-    ],
-    [[15, 'left in block B'],
-     [30, 'until block C']
-    ],
-    [
-    ],
-    [[0,'left in block C']
-    ],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-  ];
+  // var exampleSchedule = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
 
-  exampleSchedule = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-  ];
-
-  schedule = normalSchedule;
-
-  if (window.PLC) schedule = plcSchedule;
+  var schedule = window.PLC ? plcSchedule : normalSchedule;
 
   if (today.getDay() === 6 || today.getDay() === 0) {
-    alert("It's the weekend. Why are you here?");
+    alert('It\'s the weekend. Why are you here?');
     dayMessage = dayMessage + 'and it is the weekend.';
     setText(dayMessage, 'date');
     return;
   } else if (isUnusual(today)) {
-    alert("Today is an unusual day. Countdown won't work today. Sorry!");
+    alert('Today is an unusual day. Countdown won\'t work today. Sorry!');
     dayMessage = dayMessage + 'and it is a unusual day.';
     setText(dayMessage, 'date');
     return;
@@ -154,14 +109,14 @@ function start(endDate) {
 }
 
 function startTimer(schedule) {
-  now = dater();
-  scheduleArray = getNextTime(schedule, now.getHours(), now.getMinutes());
-  endTime = createDate(scheduleArray[0], scheduleArray[1], scheduleArray[3]);
+  var now = dater();
+  var scheduleArray = getNextTime(schedule, now.getHours(), now.getMinutes());
+  var endTime = createDate(scheduleArray[0], scheduleArray[1], scheduleArray[3]);
   window.stopID = setTimeout(loop, 1000, endTime, scheduleArray[2]);
 }
 
 function createDate(hour, minute, nextDay) {
-  createdDate = dater();
+  var createdDate = dater();
   createdDate.setSeconds(0);
   createdDate.setHours(hour);
   createdDate.setMinutes(minute);
@@ -170,26 +125,18 @@ function createDate(hour, minute, nextDay) {
 }
 
 function loop(endDate, message) {
-  now = dater();
+  var now = dater();
   var diff = endDate - now;
 
   var days = Math.floor(diff / (1000 * 3600 * 24));
   var hours = Math.floor(diff / 3.6e6 - days * 24);
-  var minutes = Math.floor(diff % 3.6e6 / 6e4);
-  var seconds = Math.floor(diff % 6e4 / 1000);
+  var minutes = Math.floor((diff % 3.6e6) / 6e4);
+  var seconds = Math.floor((diff % 6e4) / 1000);
   if (seconds < 0) {
     start();
-    console.log('Done');
     return;
   }
-  timeLeft =
-    days +
-    ':' +
-    ('0' + hours).slice(-2) +
-    ':' +
-    ('0' + minutes).slice(-2) +
-    ':' +
-    ('0' + seconds).slice(-2);
+  var timeLeft = days + ':' + ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2);
   if (days === 0) {
     timeLeft = timeLeft.substring(2);
     if (hours === 0) timeLeft = timeLeft.substring(3);
