d9930a1ee2de7d3d0fbc55a8cd36da991128fd3f
jpeterik12 <j.d.peterik@icloud.com>
2018-11-14 11:33:51 -0600

    Update ad.js

--- js/ad.js
+++ js/ad.js
@@ -1,20 +1,20 @@
 function ad() {
-//   const x = document.createElement('img');
-//   x.src = '/images/sorry.png';
-//   x.style.height = '90vh';
-//   x.style.width = '100vw';
-//   x.style.objectFit = 'contain';
-//   x.style.zIndex = '2147483647';
-//   x.style.display = 'block';
-//   x.onload = yeet;
-//   x.style.position = 'absolute';
-//   x.style.top = 0;
-//   document.body.appendChild(x);
-//   function deleteMe(element) {
-//     element.parentNode.removeChild(element);
-//   }
+   const x = document.createElement('img');
+  x.src = '/images/1EEEB279-F530-42B8-8FC5-6DF3DEFE9ECB.jpeg';
+  x.style.height = '90vh';
+ x.style.width = '100vw';
+ x.style.objectFit = 'contain';
+ x.style.zIndex = '2147483647';
+x.style.display = 'block';
+  x.onload = yeet;
+  x.style.position = 'absolute';
+ x.style.top = 0;
+  document.body.appendChild(x);
+  function deleteMe(element) {
+     element.parentNode.removeChild(element);
+   }
 
-//   function yeet() {
-//     setTimeout(deleteMe, 6000, x);
-//   }
+   function yeet() { 
+  setTimeout(deleteMe, 6000, x);
+  }
 }
