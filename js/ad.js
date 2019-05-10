6c12a0107586a1cd7abe222ec9d8e1ab0af9baa0
Jackson Peterik <j.d.peterik@icloud.com>
2019-05-10 08:09:11 -0500

    Add PMT ad

--- js/ad.js
+++ js/ad.js
@@ -1,21 +1,21 @@
 function ad() {
-//     const x = document.createElement('img');
-//     x.src = '/images/1EEEB279-F530-42B8-8FC5-6DF3DEFE9ECB.jpeg';
-//     x.style.height = '90vh';
-//     x.style.width = '100vw';
-//     x.style.objectFit = 'contain';
-//     x.style.zIndex = '2147483647';
-//     x.style.display = 'block';
-//     x.onload = yeet;
-//     x.style.position = 'absolute';
-//     x.style.top = 0;
-//     document.body.appendChild(x);
+    const x = document.createElement('img');
+    x.src = '/images/ads/ad2.png';
+    x.style.height = '90vh';
+    x.style.width = '100vw';
+    x.style.objectFit = 'contain';
+    x.style.zIndex = '2147483647';
+    x.style.display = 'block';
+    x.onload = yeet;
+    x.style.position = 'absolute';
+    x.style.top = 0;
+    document.body.appendChild(x);
 
-//     function deleteMe(element) {
-//         element.parentNode.removeChild(element);
-//     }
+    function deleteMe(element) {
+        element.parentNode.removeChild(element);
+    }
 
-//     function yeet() {
-//         setTimeout(deleteMe, 6000, x);
-//     }
+    function yeet() {
+        setTimeout(deleteMe, 6000, x);
+    }
 }
