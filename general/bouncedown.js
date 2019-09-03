d7de42091ed1e7e456c706423de36a8acc637289
Jackson Peterik <j.d.peterik@icloud.com>
2019-09-03 12:44:16 -0500

    Create bouncedown.js

---
+++ general/bouncedown.js
@@ -0,0 +1,8 @@
+/*jshint maxerr: 1000 */
+
+  document.body.appendChild(document.createElement('script')).src =
+    'https://www.countdownlhs.ga/js/bouncedown.js';
+  document.body.appendChild(document.createElement('script')).src =
+    'https://www.countdownlhs.ga/js/lisleschedule.js';
+  document.body.appendChild(document.createElement('script')).src =
+    'https://www.countdownlhs.ga/js/countdown.js';
