fab3f807d641a47e37b9c91d8feb1c17a80c4356
Jackson Peterik <j.d.peterik@icloud.com>
2019-09-03 12:46:45 -0500

    Update bouncedown.js

--- js/bouncedown.js
+++ js/bouncedown.js
@@ -9,7 +9,7 @@ function createClock() {
   cdlhsparent.innerHTML =
     '<p id="clock" style="z-index: inherit; font-family:monospace; padding: 0; margin: 0; font-size: 20px;">00:00</p><style>@keyframes cdlhsl-r {from {left: 0;} to {left: calc(100vw - 60px);}} @keyframes cdlhsu-d {from {top: 0;} to {top: calc(100vh - 20px);}}</style>';
   cdlhsparent.style =
-    'z-index: 2147483647; position: fixed; padding: auto; height: 20px; margin: 0; border: 1px solid #000000; background-color: white; animation:cdlhsl-r 5s linear 100 alternate, cdlhsu-d 4s linear 100 alternate;';
+    'z-index: 2147483647; position: fixed; height: 20px; padding: 0; margin: 0; border: 1px solid #000000; background-color: white; animation:cdlhsl-r 5s linear 100 alternate, cdlhsu-d 4s linear 100 alternate;';
   cdlhsparent.id = 'cdlhsparent';
 
   document.body.appendChild(cdlhsparent);
