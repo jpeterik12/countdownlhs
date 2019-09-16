8e5ec029d11945c15a53f9320362bea1d8d03801
Jackson Peterik <j.d.peterik@icloud.com>
2019-09-16 12:45:56 -0500

    Update saving.js

--- js/saving.js
+++ js/saving.js
@@ -1,40 +1,14 @@
-var customCountdown = document.getElementById('customCountdown');
-var delayInput = document.getElementById('delayInput');
-var notepad = document.getElementById('notepad');
+const inputs = [...document.getElementsByTagName('input'), ...document.getElementsByTagName('textarea')]
 
-if (window.localStorage.getItem('custom')) {
-  customCountdown.value = localStorage.getItem('custom');
-}
-
-if (window.localStorage.getItem('delay')) {
-  delayInput.value = localStorage.getItem('delay');
-}
-
-if (window.localStorage.getItem('notepad')) {
-  notepad.value = localStorage.getItem('notepad');
-}
-
-delayInput.onkeypress = function (e) {
-  if (e.keyCode == 13) {
-    document.getElementById('delayButton').click();
-  }
+function saveAll() {
+  for (input of inputs) localStorage.setItem(input.id, input.value);
 };
 
-customCountdown.onkeypress = function (e) {
-  if (e.keyCode == 13) {
-    document.getElementById('customButton').click();
-  }
-};
-
-customCountdown.oninput = saveAll;
-delayInput.oninput = saveAll;
-notepad.oninput = saveAll;
+for (input of inputs) {
+  input.value = localstorage.getItem(input.id) || ''; 
+  input.oninput = saveAll;
+}
 window.onunload = saveAll;
 
-var saveAll = function () {
-  localStorage.setItem('custom', customCountdown.value);
-  localStorage.setItem('notepad', notepad.value);
-  localStorage.setItem('delay', delayInput.value);
-};
 
 setInterval(saveAll, 2000);
