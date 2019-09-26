da5a64eaaa210d8092b55caf258b091b004d5497
Jackson Peterik <j.d.peterik@icloud.com>
2019-09-26 12:37:45 -0500

    Update ad.js

--- js/ad.js
+++ js/ad.js
@@ -2,15 +2,15 @@
 function ad() {
   const x = document.createElement('img');
   x.src = '/images/ads/ad2.png';
-  x.style.height = '70vh';
-  x.style.width = '15vw';
+  x.style.height = '80vh';
+  x.style.width = '20vw';
   x.style.objectFit = 'contain';
   x.style.zIndex = '2147483647';
   x.style.display = 'block';
 //   x.onload = yeet;
   x.style.position = 'absolute';
   x.style.top = '0';
-  x.style.left = '70vw'
+  x.style.left = '60vw'
   document.body.appendChild(x);
 
   function deleteMe(element) {
