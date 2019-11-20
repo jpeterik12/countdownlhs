dae6f29a33fed2134e34f7b23942a961fd5a677f
Jackson Peterik <j.d.peterik@icloud.com>
2019-11-20 09:07:45 -0600

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -106,7 +106,7 @@ function startTimer(endDate) {
         window.location.hostname === 'www.countdownlhs.ga' ||
         window.location.hostname === 'localhost' ||
         window.location.hostname === 'countdown.jpeterik12.repl.co' ||
-        windwo.location.hostname === 'jpsys.jpeterik12.repl.co'
+        window.location.hostname === 'jpsys.jpeterik12.repl.co'
       )
         document.title = timeMessage;
       // console.log(timeString + '\n' + timeMessage);
