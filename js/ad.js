2c380b0db4814acdc4c236e91eb61bce5d891131
JP12 <j.d.peterik@icloud.com>
2020-02-26 20:39:50 -0600

    Josh

--- js/ad.js
+++ js/ad.js
@@ -1,26 +1,28 @@
 /* eslint-disable no-unused-vars */
 function ad () {
+  if (new Date() > new Date('2/28/2020')) return;
   const x = document.createElement('a');
-  x.target = '_blank';
-  x.href = 'https://www.caffedimoda.com/';
+  // x.target = '_blank';
+  // x.href = 'https://www.caffedimoda.com/';
   x.style.display = 'block';
   x.style.position = 'absolute';
   x.style.bottom = '10vh';
   x.style.right = '10vw';
   x.style.width = '80vw';
   x.style.height = '80vh';
-  x.style.transition = 'all 1s';
+  // x.style.transition = 'all 1s';
 
   const y = document.createElement('img');
-  y.src = '/images/ads/caffe_di_moda.png';
+  y.src = '/images/ads/josh.png';
   y.style.objectFit = 'contain';
   y.style.zIndex = '2147483647';
-  y.onload = yeet;
+  // y.onload = yeet;
   y.style.height = '100%';
   y.style.width = '100%';
 
   x.appendChild(y);
-//   document.body.appendChild(x);
+  moveMe(x)
+  document.body.appendChild(x);
 
   function deleteMe (element) {
     element.parentNode.removeChild(element);
@@ -33,8 +35,8 @@ function ad () {
     element.style.width = '18vw';
   }
 
-  function yeet () {
-    // setTimeout(deleteMe, 6000, x);
-    setTimeout(moveMe, 6000, x);
-  }
+  // function yeet () {
+  // setTimeout(deleteMe, 6000, x);
+  // setTimeout(moveMe, 6000, x);
+  // }
 }
