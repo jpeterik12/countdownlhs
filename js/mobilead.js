a1e42f4c53a13b183624eae6df73684a11f164e4
JP12 <j.d.peterik@icloud.com>
2020-02-26 20:42:15 -0600

    mobile as well

--- js/mobilead.js
+++ js/mobilead.js
@@ -1,18 +1,19 @@
 /* eslint-disable no-unused-vars */
 function ad () {
+  if (new Date() > new Date('2/28/2020')) return;
   const x = document.createElement('a');
-  x.target = '_blank';
-  x.href = 'https://www.caffedimoda.com/';
+  // x.target = '_blank';
+  // x.href = 'https://www.caffedimoda.com/';
   x.style.display = 'block';
   x.style.position = 'absolute';
   x.style.bottom = '10vh';
   x.style.right = '10vw';
   x.style.width = '80vw';
   x.style.height = '80vh';
-  x.style.transition = 'all 1s';
+  // x.style.transition = 'all 1s';
 
   const y = document.createElement('img');
-  y.src = '/images/ads/caffe_di_moda.png';
+  y.src = '/images/ads/josh.png';
   y.style.objectFit = 'contain';
   y.style.zIndex = '2147483647';
   y.onload = yeet;
@@ -20,7 +21,7 @@ function ad () {
   y.style.width = '100%';
 
   x.appendChild(y);
-//   document.body.appendChild(x);
+  document.body.appendChild(x);
 
   function deleteMe (element) {
     element.parentNode.removeChild(element);
